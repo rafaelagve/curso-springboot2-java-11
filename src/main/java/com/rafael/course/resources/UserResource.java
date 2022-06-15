e592e875017af24e303c5a0a42e1d1abb9ea684e
Rafael AGV <rafael.agv@protonmail.com>
2022-06-15 17:44:01 -0300

    User entity and resource

---
+++ src/main/java/com/rafael/course/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.rafael.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rafael.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll()
+	{
+		User u = new User(1L, "Maria", "Maria@gmail.com", "41143214", "gegreg453" );
+		
+		return ResponseEntity.ok().body(u);
+	}
+
+}
