a418192c790f03ea0aabd02081ea3395b1c13602
Rafael AGV <rafael.agv@protonmail.com>
2022-06-16 11:26:18 -0300

    User Repository and TestConfig

---
+++ src/main/java/com/rafael/course/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.rafael.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rafael.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+
+
+}
