32ea97e132e066e227402be9a29a8fce188020fe
Rafael AGV <rafael.agv@protonmail.com>
2022-06-17 16:46:28 -0300

    Product class

---
+++ src/main/java/com/rafael/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.rafael.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rafael.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+}
---
+++ src/main/java/com/rafael/course/services/ProductService.java
@@ -0,0 +1,30 @@
+package com.rafael.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rafael.course.entities.Product;
+import com.rafael.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll()
+	{
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id)
+	{
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+		
+	}
+
+}
