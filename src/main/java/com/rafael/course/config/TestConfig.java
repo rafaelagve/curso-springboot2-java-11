099d95dbe87b042960ba35262e1f7a7523292d68
Rafael AGV <rafael.agv@protonmail.com>
2022-06-23 12:22:42 -0300

    Payment class

--- src/main/java/com/rafael/course/config/TestConfig.java
+++ src/main/java/com/rafael/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.rafael.course.entities.Category;
 import com.rafael.course.entities.Order;
 import com.rafael.course.entities.OrderItem;
+import com.rafael.course.entities.Payment;
 import com.rafael.course.entities.Product;
 import com.rafael.course.entities.User;
 import com.rafael.course.entities.enums.OrderStatus;
@@ -82,6 +83,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3));
 		
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 	
 	
